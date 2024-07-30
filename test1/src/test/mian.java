5c2049d9d133c4680ae73e8dd3378c87d792a5ea
猴小陶 <2313555401@qq.com>
2024-07-30 21:04:35 +0800

    输出“新建1”

--- test1/src/test/mian.java
+++ test1/src/test/mian.java
@@ -2,8 +2,8 @@ package test;
 
 public class mian {
     public static void main(String[] args) {
-        System.out.println("hello wlord");
-        System.out.println("hello wlord2");
-
+        System.out.println("hello world");
+        System.out.println("hello world2");
+        System.out.println("新建1");
     }
 }
